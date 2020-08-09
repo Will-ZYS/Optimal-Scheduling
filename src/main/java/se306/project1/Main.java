348b4940e35fa05f74e1b0bdb4b94683e7eea9de
Tommy Shi <tommyshi19991130@gmail.com>
2020-08-09 21:03:40 +1200

    Initial commit

---
+++ src/main/java/se306/project1/Main.java
@@ -0,0 +1,7 @@
+package se306.project1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, SOFTENG306 project 1!");
+    }
+}
---
+++ src/test/java/se306/project1/MainTest.java
@@ -0,0 +1,11 @@
+package se306.project1;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        Main.main(new String[]{});
+    }
+}
