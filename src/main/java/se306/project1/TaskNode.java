42ca455b306f0a7c79a12c90c3501eee178ac7ac
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-08-11 22:34:05 +1200

    [LUCK-012] create child task nodes
    Issue:
    To generate child solutions, we need to first find out all child task nodes that can be executed.
    
    Solution:
    TaskNode contains incomingEdges and outgoingEdges, which should be produced by inputReader.
    By tracking the incomingEdges, we can know all the child task nodes.
    By tracking the outgoingEdges of each child task nodes, we can know if this child task node is executable.
    
    Risk:
    Currently each SolutionNode only contains one processor and current task. However, when calculating the start time of a new task, we need to find all its parent task nodes, and figuire out the processor that each parent task node is in. This is unable to achieve with current design.
    
    Reviewed by:
    Will, Martin

--- src/main/java/se306/project1/TaskNode.java
+++ src/main/java/se306/project1/TaskNode.java
@@ -1,8 +1,12 @@
 package se306.project1;
 
+import java.util.List;
+
 public class TaskNode {
     private int _weight;
     private String _name;
+    private List<DataTransferEdge> _incomingEdges;
+    private List<DataTransferEdge> _outgoingEdges;
 
     public TaskNode(int weight, String name) {
         _weight = weight;
@@ -14,4 +18,11 @@ public class TaskNode {
     }
 
 
+    public List<DataTransferEdge> getOutgoingEdges() {
+        return _outgoingEdges;
+    }
+
+    public List<DataTransferEdge> getIncomingEdges() {
+        return _incomingEdges;
+    }
 }
