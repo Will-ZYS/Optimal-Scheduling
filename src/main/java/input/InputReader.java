02b790741e7edb51f8e994611aaf48e408990361
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-08-17 21:59:17 +1200

    [LUCK-012] reformat input reader and schedular file

--- src/main/java/input/InputReader.java
+++ src/main/java/input/InputReader.java
@@ -14,122 +14,135 @@ import java.util.regex.Pattern;
 
 public class InputReader {
 
-    private final String _pathToDotFile;
-    private final int _numOfProcessor;
-    private String _graphName = "exampleGraph";
+	private final String PATH_TO_DOT_FILE;
+	private final int NUM_OF_PROCESSOR;
+	private String _graphName = "exampleGraph";
 
-    private final LinkedHashMap<String, String> _inputRowsRaw = new LinkedHashMap<>();
+	// storing the order of lines of the input file into a linked hash map for output
+	private final LinkedHashMap<String, String> INPUT_ROWS_RAW = new LinkedHashMap<>();
 
-    public InputReader ( String path, int processors ){
-        _pathToDotFile = path;
-        _numOfProcessor = processors;
-    }
+	public InputReader(String path, int processors) {
+		PATH_TO_DOT_FILE = path;
+		NUM_OF_PROCESSOR = processors;
+	}
 
-    public SolutionTree readInputFile() throws IOException {
+	/**
+	 * Reading the input .dot file
+	 *
+	 * @return a SolutionTree object that contains a list of processors and tasks
+	 * @throws IOException when the given input file path is invalid
+	 */
+	public SolutionTree readInputFile() throws IOException {
 
-        // read the file with provided path
-        FileReader dotFile = new FileReader( _pathToDotFile );
-        BufferedReader brFile = new BufferedReader(dotFile);
+		// read the file with provided path
+		FileReader dotFile = new FileReader(PATH_TO_DOT_FILE);
+		BufferedReader brFile = new BufferedReader(dotFile);
 
-        Map<String, TaskNode> taskNodeMap = new HashMap<>(); // map between task name and its TaskNode object
+		// map between task name and its TaskNode object
+		Map<String, TaskNode> taskNodeMap = new HashMap<>();
 
-        // read the file line by line and get desired information from each line
-        String line;
-        while ((line = brFile.readLine()) != null) {
+		// read the file line by line and get desired information from each line
+		String line;
+		while ((line = brFile.readLine()) != null) {
 
-            // ignore the line without the symbol '['
-            if (!line.contains("[")) {
-                // if the line is the first line which contains the graph name
-                if (line.contains("\"")) {
-                    Pattern doubleQuotes = Pattern.compile("\"([^\"]*)\"");
-                    Matcher findDoubleQuotes = doubleQuotes.matcher(line);
-                    while (findDoubleQuotes.find()) {
-                        _graphName = findDoubleQuotes.group(1);
-                    }
-                }
-                continue;
-            }
+			// ignore the line without the symbol '['
+			if (! line.contains("[")) {
+				// if the line is the first line which contains the graph name
+				if (line.contains("\"")) {
+					Pattern doubleQuotes = Pattern.compile("\"([^\"]*)\"");
+					Matcher findDoubleQuotes = doubleQuotes.matcher(line);
+					while (findDoubleQuotes.find()) {
+						_graphName = findDoubleQuotes.group(1);
+					}
+				}
+				continue;
+			}
 
-            // get the weight and task node name from lines
-            String[] lineArray = line.split("\\[");
-            String attributeInfo = lineArray[0].trim();
-            String weightInfo = lineArray[1].trim().split("=")[1];
-            int weight = Integer.parseInt(weightInfo.substring(0, weightInfo.length()-2));
+			// get the weight and task node name from lines
+			String[] lineArray = line.split("\\[");
+			String attributeInfo = lineArray[0].trim();
+			String weightInfo = lineArray[1].trim().split("=")[1];
+			int weight = Integer.parseInt(weightInfo.substring(0, weightInfo.length() - 2));
 
-            if (attributeInfo.contains("->")) { // process as edge
-                // Store as an edge in LinkedMap (preserved order)
-                _inputRowsRaw.put(attributeInfo, "["+lineArray[1]);
+			if (attributeInfo.contains("->")) { // process as edge
+				// Store as an edge in LinkedMap (preserved order)
+				INPUT_ROWS_RAW.put(attributeInfo, "[" + lineArray[1]);
 
-                String[] nodeInfo = attributeInfo.split("->");
-                String sourceName = nodeInfo[0].trim();
-                String destinationName = nodeInfo[1].trim();
+				String[] nodeInfo = attributeInfo.split("->");
+				String sourceName = nodeInfo[0].trim();
+				String destinationName = nodeInfo[1].trim();
 
-                // get or create the source and destination node
-                TaskNode sourceNode;
-                TaskNode destinationNode;
-                if (taskNodeMap.containsKey(sourceName) && taskNodeMap.containsKey(destinationName)) {
-                    sourceNode = taskNodeMap.get(sourceName);
-                    destinationNode = taskNodeMap.get(destinationName);
-                } else if (taskNodeMap.containsKey(sourceName)) {
-                    sourceNode = taskNodeMap.get(sourceName);
-                    destinationNode = new TaskNode(destinationName);
-                    taskNodeMap.put(destinationName, destinationNode);
-                } else if (taskNodeMap.containsKey(destinationName)) {
-                    sourceNode = new TaskNode(sourceName);
-                    taskNodeMap.put(sourceName, sourceNode);
-                    destinationNode = taskNodeMap.get(destinationName);
-                } else {
-                    sourceNode = new TaskNode(sourceName);
-                    destinationNode = new TaskNode(destinationName);
-                    taskNodeMap.put(sourceName, sourceNode);
-                    taskNodeMap.put(destinationName,destinationNode);
-                }
+				// get or create the source and destination node
+				TaskNode sourceNode;
+				TaskNode destinationNode;
+				if (taskNodeMap.containsKey(sourceName) && taskNodeMap.containsKey(destinationName)) {
+					sourceNode = taskNodeMap.get(sourceName);
+					destinationNode = taskNodeMap.get(destinationName);
+				} else if (taskNodeMap.containsKey(sourceName)) {
+					sourceNode = taskNodeMap.get(sourceName);
+					destinationNode = new TaskNode(destinationName);
+					taskNodeMap.put(destinationName, destinationNode);
+				} else if (taskNodeMap.containsKey(destinationName)) {
+					sourceNode = new TaskNode(sourceName);
+					taskNodeMap.put(sourceName, sourceNode);
+					destinationNode = taskNodeMap.get(destinationName);
+				} else {
+					sourceNode = new TaskNode(sourceName);
+					destinationNode = new TaskNode(destinationName);
+					taskNodeMap.put(sourceName, sourceNode);
+					taskNodeMap.put(destinationName, destinationNode);
+				}
 
-                // create the DataTransferEdge and write to adjacency list
-                DataTransferEdge edge = new DataTransferEdge(sourceNode, weight);
-                destinationNode.addIncomingEdge(edge);
+				// create the DataTransferEdge and write to adjacency list
+				DataTransferEdge edge = new DataTransferEdge(sourceNode, weight);
+				destinationNode.addIncomingEdge(edge);
 
-            } else { // process as node
-                // Store as an node in LinkedMap (preserved order)
-                String nodeData = lineArray[1].trim().replace("];","");
-                _inputRowsRaw.put(attributeInfo,nodeData);
+			} else {
+				// process the current line as a task node
+				// Store as an node in LinkedMap (preserved order)
+				String nodeData = lineArray[1].trim().replace("];", "");
+				INPUT_ROWS_RAW.put(attributeInfo, nodeData);
 
-                if (!taskNodeMap.containsKey(attributeInfo)) {
-                    TaskNode node = new TaskNode(weight, attributeInfo);
-                    taskNodeMap.put(attributeInfo, node);
-                } else {
-                    taskNodeMap.get(attributeInfo).setWeight(weight);
-                }
-            }
+				if (! taskNodeMap.containsKey(attributeInfo)) {
+					TaskNode node = new TaskNode(weight, attributeInfo);
+					taskNodeMap.put(attributeInfo, node);
+				} else {
+					taskNodeMap.get(attributeInfo).setWeight(weight);
+				}
+			}
 
-        }
-        dotFile.close();
+		}
+		dotFile.close();
 
-        // list of tasks
-        List<TaskNode> taskList = new ArrayList<>(taskNodeMap.values());
+		// list of tasks
+		List<TaskNode> taskList = new ArrayList<>(taskNodeMap.values());
 
-        // new a solution tree object which will be used later
-        return new SolutionTree(taskList, generateProcessors());
-    }
+		// new a solution tree object which will be used later
+		return new SolutionTree(taskList, generateProcessors());
+	}
 
-    // generate a list of processors, the id starts with 1
-    private List<Processor> generateProcessors() {
+	/**
+	 * Generate a list of empty processors, the id starts with 1
+	 *
+	 * @return a list of processors
+	 */
+	private List<Processor> generateProcessors() {
 
-        List<Processor> processorList = new ArrayList<>();
+		List<Processor> processorList = new ArrayList<>();
 
-        for ( int i = 1; i<= _numOfProcessor; i++ ){
-            processorList.add(new Processor(i));
-        }
+		for (int i = 1; i <= NUM_OF_PROCESSOR; i++) {
+			processorList.add(new Processor(i));
+		}
 
-        return processorList;
-    }
+		return processorList;
+	}
 
-    public LinkedHashMap<String, String> getInputRowsRaw() {
-        return _inputRowsRaw;
-    }
+	public LinkedHashMap<String, String> getInputRowsRaw() {
+		return INPUT_ROWS_RAW;
+	}
 
-    public String getGraphName() {
-        return _graphName;
-    }
+	public String getGraphName() {
+		return _graphName;
+	}
 
 }
--- src/main/java/main/Scheduler.java
+++ src/main/java/main/Scheduler.java
@@ -8,91 +8,93 @@ import output.OutputGenerator;
 import java.io.IOException;
 
 public class Scheduler {
-    private static int _numOfProcessor = 0;
-    private static String _outputName;
+	private static int _numOfProcessor = 0;
+	private static String _outputName;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scheduler scheduler = new Scheduler();
-        scheduler.readUserInput(args);
+		Scheduler scheduler = new Scheduler();
+		scheduler.readUserInput(args);
 
-        try {
-            // read the input file and return it as a solutionTree object
-            InputReader inputFile = new InputReader(args[0], _numOfProcessor);
+		try {
+			// read the input file and return it as a solutionTree object
+			InputReader inputFile = new InputReader(args[0], _numOfProcessor);
 
-            SolutionTree solutionTree = inputFile.readInputFile();
+			SolutionTree solutionTree = inputFile.readInputFile();
 
-            // get the graphName from the input file
-            String graphName = inputFile.getGraphName();
+			// get the graphName from the input file
+			String graphName = inputFile.getGraphName();
 
-            SolutionNode bestSolution = solutionTree.findOptimalSolution();
+			SolutionNode bestSolution = solutionTree.findOptimalSolution();
 
-            // Generating output
-            OutputGenerator outputGenerator = new OutputGenerator(bestSolution, _outputName, inputFile.getInputRowsRaw(), graphName);
-            outputGenerator.writeOutput();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			// Generating output
+			OutputGenerator outputGenerator = new OutputGenerator(bestSolution, _outputName,
+					                                              inputFile.getInputRowsRaw(), graphName);
+			outputGenerator.writeOutput();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * This function processes user's command inputs
-     * @param args user inputs
-     */
-    private void readUserInput(String[] args) {
-        // Check the number of command line argument is greater than 2
-        if (args.length < 2) {
-            System.err.println("Usage: java -jar scheduler.jar INPUT.dot P [-p N] [-v] [-o OUTPUT]");
-            System.exit(1);
-        }
+	/**
+	 * This function processes user's command inputs
+	 *
+	 * @param args user inputs
+	 */
+	private void readUserInput(String[] args) {
+		// Check the number of command line argument is greater than 2
+		if (args.length < 2) {
+			System.err.println("Usage: java -jar scheduler.jar INPUT.dot P [-p N] [-v] [-o OUTPUT]");
+			System.exit(1);
+		}
 
-        // check if the first argument is a valid path to a .dot file
-        int args0Length = args[0].length();
-        if (args0Length < 4 || !args[0].substring(args0Length - 4).equals(".dot")) {
-            System.err.println("Usage: java -jar scheduler.jar INPUT.dot P [-p N] [-v] [-o OUTPUT]");
-            System.err.println("INPUT.dot needs to be a valid path to the input file with .dot extension");
-            System.exit(1);
-        }
-        String inputName = args[0].substring(0, args0Length - 4);
+		// check if the first argument is a valid path to a .dot file
+		int args0Length = args[0].length();
+		if (args0Length < 4 || ! args[0].substring(args0Length - 4).equals(".dot")) {
+			System.err.println("Usage: java -jar scheduler.jar INPUT.dot P [-p N] [-v] [-o OUTPUT]");
+			System.err.println("INPUT.dot needs to be a valid path to the input file with .dot extension");
+			System.exit(1);
+		}
+		String inputName = args[0].substring(0, args0Length - 4);
 
-        // check if the second argument is a positive integer
-        boolean isPositiveInt = false;
-        try {
-            _numOfProcessor = Integer.parseInt(args[1]);
-            isPositiveInt = _numOfProcessor > 0;
-        } catch (NumberFormatException ignored) {
+		// check if the second argument is a positive integer
+		boolean isPositiveInt = false;
+		try {
+			_numOfProcessor = Integer.parseInt(args[1]);
+			isPositiveInt = _numOfProcessor > 0;
+		} catch (NumberFormatException ignored) {
 
-        }
-        if (!isPositiveInt) {
-            System.err.println("Usage: java -jar scheduler.jar INPUT.dot P [-p N] [-v] [-o OUTPUT]");
-            System.err.println("The number of processors, P, needs to be a positive integer");
-            System.exit(1);
-        }
+		}
+		if (! isPositiveInt) {
+			System.err.println("Usage: java -jar scheduler.jar INPUT.dot P [-p N] [-v] [-o OUTPUT]");
+			System.err.println("The number of processors, P, needs to be a positive integer");
+			System.exit(1);
+		}
 
-        // loop through option and check stuffs
-        _outputName = inputName + "-output";
-        for (int i = 2; i < args.length; i++) {
-            switch (args[i]) {
-                case "-v":
-                    System.out.println("Sorry, the visualiser has not been implemented yet, you can find the " +
+		// loop through option and check stuffs
+		_outputName = inputName + "-output";
+		for (int i = 2; i < args.length; i++) {
+			switch (args[i]) {
+				case "-v":
+					System.out.println("Sorry, the visualiser has not been implemented yet, you can find the " +
                                        "result in the output file");
-                    break;
-                case "-p":
-                    System.out.println("Sorry, the parallel version has not been implemented yet, " +
-                                       "the algorithm will be run in sequential");
-                    i++;
-                    break;
-                case "-o":
-                    if (i == args.length - 1) {
-                        System.err.println("Usage: java -jar scheduler.jar INPUT.dot P [-p N] [-v] [-o OUTPUT]");
-                        System.err.println("You need to specify the output file name");
-                        System.exit(1);
-                    }
-                    _outputName = args[i + 1];
-                    i++;
-                    break;
-            }
-        }
+					break;
+				case "-p":
+					System.out.println("Sorry, the parallel version has not been implemented yet, " +
+							           "the algorithm will be run in sequential");
+					i++;
+					break;
+				case "-o":
+					if (i == args.length - 1) {
+						System.err.println("Usage: java -jar scheduler.jar INPUT.dot P [-p N] [-v] [-o OUTPUT]");
+						System.err.println("You need to specify the output file name");
+						System.exit(1);
+					}
+					_outputName = args[i + 1];
+					i++;
+					break;
+			}
+		}
 
-    }
+	}
 }
