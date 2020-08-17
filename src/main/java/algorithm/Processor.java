d7e78599ed50d883824521c29f46e98c0557e5ca
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-08-17 22:02:13 +1200

    [LUCK-012] reformat processor class

--- src/main/java/algorithm/Processor.java
+++ src/main/java/algorithm/Processor.java
@@ -4,41 +4,46 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Processor {
-    private final Map<TaskNode, Integer> _tasks;
-    private int _endTime;
-    private final int PROCESSOR_ID;
+	// Integer: starting time of a specific task in this processor
+	private final Map<TaskNode, Integer> TASKS;
+	private int _endTime;
+	private final int PROCESSOR_ID;
 
-    public Processor (int id) {
-        _tasks = new HashMap<>();
-        _endTime = 0;
-        PROCESSOR_ID = id;
-    }
+	public Processor(int id) {
+		TASKS = new HashMap<>();
+		_endTime = 0;
+		PROCESSOR_ID = id;
+	}
 
-    // Create a copy of a given processor
-    public Processor(Processor processor) {
-        PROCESSOR_ID = processor.getID();
-        _tasks = new HashMap<>(processor.getTasks());
-        _endTime = processor.getEndTime();
-    }
+	/**
+	 * Create a copy of a given processor
+	 *
+	 * @param processor a processor that to copy from
+	 */
+	public Processor(Processor processor) {
+		PROCESSOR_ID = processor.getID();
+		TASKS = new HashMap<>(processor.getTasks());
+		_endTime = processor.getEndTime();
+	}
 
-    public Map<TaskNode, Integer> getTasks() {
-        return _tasks;
-    }
+	public Map<TaskNode, Integer> getTasks() {
+		return TASKS;
+	}
 
-    public void addTask(TaskNode task, int startTime) {
-        _tasks.put(task, startTime);
-        _endTime = startTime + task.getWeight();
-    }
+	public void addTask(TaskNode task, int startTime) {
+		TASKS.put(task, startTime);
+		_endTime = startTime + task.getWeight();
+	}
 
-    public int getEndTime() {
-        return _endTime;
-    }
+	public int getEndTime() {
+		return _endTime;
+	}
 
-    public void setEndTime(int _endTime) {
-        this._endTime = _endTime;
-    }
+	public void setEndTime(int _endTime) {
+		this._endTime = _endTime;
+	}
 
-    public int getID(){
-        return PROCESSOR_ID;
-    }
+	public int getID() {
+		return PROCESSOR_ID;
+	}
 }
