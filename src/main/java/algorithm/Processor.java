0d924b9088d1e33cd824c2e1b0a1bfb01cd5bc30
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-08-26 19:52:08 +1200

    [LUCK-012] refactor heuristic equation
    Issue:
    
    In the original implementation, every time we calculate the lower bound, we need to loop through all allocated tasks.
    This significantly increases the runtime.
    
    Solution:
    
    Add another filed _criticalPath in the Processor which is the max(start time of an allocated task + bottom level of that task).
    This field is updated every time a new task is added and it only stores the maxmimum value.
    Therefore, when calculating the lower bound, we only need to loop through each processor, no longer need to loop through all allocated tasks.
    
    Risk:
    /
    
    Reviewed by:
    Will, Lucas, Martin, Tommy, Kevin

--- src/main/java/algorithm/Processor.java
+++ src/main/java/algorithm/Processor.java
@@ -9,11 +9,13 @@ public class Processor {
 	private int _endTime;
 	private final int PROCESSOR_ID;
 	private int _idleTime;
+	private int _critialPath;
 
 	public Processor(int id) {
 		TASKS = new HashMap<>();
 		_endTime = 0;
 		_idleTime = 0;
+		_critialPath = 0;
 		PROCESSOR_ID = id;
 	}
 
@@ -26,6 +28,8 @@ public class Processor {
 		PROCESSOR_ID = processor.getID();
 		TASKS = new HashMap<>(processor.getTasks());
 		_endTime = processor.getEndTime();
+		_idleTime = processor.getIdleTime();
+		_critialPath = processor.getCriticalPath();
 	}
 
 	public Map<TaskNode, Integer> getTasks() {
@@ -36,6 +40,13 @@ public class Processor {
 		TASKS.put(task, startTime);
 		_idleTime += startTime - _endTime;
 		_endTime = startTime + task.getWeight();
+		if (startTime + task.getBottomLevel() > _critialPath) {
+			_critialPath = startTime + task.getBottomLevel();
+		}
+	}
+
+	public int getCriticalPath() {
+		return _critialPath;
 	}
 
 	public int getEndTime() {
