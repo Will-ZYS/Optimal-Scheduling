bfa1b1f0bb3f1632175af78671d43675da65f6a4
Tommy Shi <tommyshi19991130@gmail.com>
2020-08-29 10:32:52 +1200

    [LUCK-013] Check lower bound before putting node to the stack

--- src/main/java/algorithm/SolutionRecursiveAction.java
+++ src/main/java/algorithm/SolutionRecursiveAction.java
@@ -53,40 +53,38 @@ public class SolutionRecursiveAction extends RecursiveAction {
 		while (!_workload.isEmpty()) {
 			SolutionNode solutionNode = _workload.pop();
 
-			// check the lower bound (estimation) of this node
-			int bestTime = PARALLEL_SOLUTION_TREE.getBestTime();
-			if (solutionNode.getLowerBound(TOTAL_TASK_WEIGHT) < bestTime) {
-				// get the unvisited nodes of this solutionNode
-				List<TaskNode> unvisitedTaskNodes = solutionNode.getUnvisitedTaskNodes();
+			// get the unvisited nodes of this solutionNode
+			List<TaskNode> unvisitedTaskNodes = solutionNode.getUnvisitedTaskNodes();
 
-				// if this solutionNode still has unvisited task node
-				if (! unvisitedTaskNodes.isEmpty()) {
-					// loop through all the unvisited task nodes
-					for (TaskNode taskNode : unvisitedTaskNodes) {
-						// if this task node can be used to create a partial solution
-						if (solutionNode.canCreateNode(taskNode)) {
+			// if this solutionNode still has unvisited task node
+			if (! unvisitedTaskNodes.isEmpty()) {
+				// loop through all the unvisited task nodes
+				for (TaskNode taskNode : unvisitedTaskNodes) {
+					// if this task node can be used to create a partial solution
+					if (solutionNode.canCreateNode(taskNode)) {
 
-							// find the possible start time for this taskNode
-							solutionNode.calculateStartTime(taskNode);
+						// find the possible start time for this taskNode
+						solutionNode.calculateStartTime(taskNode);
 
-							// loop through all processors
-							int numProcessors = PARALLEL_SOLUTION_TREE.getNumberOfProcessors();
-							for (int i = 0; i < numProcessors; i++) {
-								// call create child nodes by giving the id of processor as a parameter
-								// get the returned child solutionNodes
-								SolutionNode childSolutionNode = solutionNode.createChildNode(taskNode, i);
+						// loop through all processors
+						int numProcessors = PARALLEL_SOLUTION_TREE.getNumberOfProcessors();
+						for (int i = 0; i < numProcessors; i++) {
+							// call create child nodes by giving the id of processor as a parameter
+							// get the returned child solutionNodes
+							SolutionNode childSolutionNode = solutionNode.createChildNode(taskNode, i);
 
-								// put new child into the stack
+							// put new child into the stack
+							if (childSolutionNode.getLowerBound(TOTAL_TASK_WEIGHT) < PARALLEL_SOLUTION_TREE.getBestTime()) {
 								_workload.push(childSolutionNode);
 							}
 						}
 					}
-				} else {
-					// compare the actual time of the leaf to the best time
-					if (solutionNode.getEndTime() < bestTime) {
-						PARALLEL_SOLUTION_TREE.setBestTime(solutionNode.getEndTime());
-						PARALLEL_SOLUTION_TREE.setBestSolution(solutionNode);
-					}
+				}
+			} else {
+				// compare the actual time of the leaf to the best time
+				if (solutionNode.getEndTime() < PARALLEL_SOLUTION_TREE.getBestTime()) {
+					PARALLEL_SOLUTION_TREE.setBestTime(solutionNode.getEndTime());
+					PARALLEL_SOLUTION_TREE.setBestSolution(solutionNode);
 				}
 			}
 
