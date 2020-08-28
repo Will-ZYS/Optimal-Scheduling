cb22a7a528b4276f0ce18ead7bd69df3a56da047
Tommy Shi <tommyshi19991130@gmail.com>
2020-08-28 20:19:07 +1200

    [LUCK-013] Make a SolutionTree interface for sequential and parallel

--- src/main/java/algorithm/ParallelSolutionTree.java
+++ src/main/java/algorithm/ParallelSolutionTree.java
@@ -1,32 +1,27 @@
 package algorithm;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
 
 public class ParallelSolutionTree extends SolutionTree {
-	private int _bestTime = Integer.MAX_VALUE; // best time
-	private SolutionNode _bestSolution;
-	private final List<TaskNode> TASKS;
-	private final SolutionNode ROOT;
-	private final int NUMBER_OF_PROCESSORS;
-	private final int TOTAL_TASK_WEIGHT;
 	private final int NUM_CORES;
 	public static ForkJoinPool _forkJoinPool;
 
 	public ParallelSolutionTree(List<TaskNode> allTasks, List<Processor> processors, int numCores) {
 		super(allTasks, processors);
-		ROOT = new SolutionNode(processors, allTasks);
-		TASKS = allTasks;
-		NUMBER_OF_PROCESSORS = processors.size();
-		int total_task_weight = 0;
-		for (TaskNode taskNode : allTasks) {
-			total_task_weight += taskNode.getWeight();
-		}
-		TOTAL_TASK_WEIGHT = total_task_weight;
 		NUM_CORES = numCores;
+	}
+
+	@Override
+	public SolutionNode findOptimalSolution() {
+		if (! TASKS.isEmpty()) {
+			DFSBranchAndBoundAlgorithm(ROOT);
+		} else {
+			_bestTime = 0;
+			_bestSolution = ROOT;
+		}
+		return _bestSolution;
 	}
 
 	/**
@@ -34,51 +29,13 @@ public class ParallelSolutionTree extends SolutionTree {
 	 *
 	 * @param solutionNode the node to perform DFS branch and bound
 	 */
-	private void DFSBranchAndBoundAlgorithm(SolutionNode solutionNode) {
+	protected void DFSBranchAndBoundAlgorithm(SolutionNode solutionNode) {
 		Stack<SolutionNode> stack = new Stack<>();
 		stack.push(ROOT);
 
 		_forkJoinPool = new ForkJoinPool(NUM_CORES);
 		ScheduleRecursiveAction schedule = new ScheduleRecursiveAction(stack, this);
 		_forkJoinPool.invoke(schedule);
-
-		// check the lower bound (estimation) of this node
-		if (solutionNode.getLowerBound(TOTAL_TASK_WEIGHT) < _bestTime) {
-
-			// get the unvisited nodes of this solutionNode
-			List<TaskNode> unvisitedTaskNodes = solutionNode.getUnvisitedTaskNodes();
-
-			// if this solutionNode still has unvisited task node
-			if (! unvisitedTaskNodes.isEmpty()) {
-				// loop through all the unvisited task nodes
-				for (TaskNode taskNode : unvisitedTaskNodes) {
-					// if this task node can be used to create a partial solution
-					if (solutionNode.canCreateNode(taskNode)) {
-
-						// find the possible start time for this taskNode
-						solutionNode.calculateStartTime(taskNode);
-
-						// loop through all processors
-						for (int i = 0; i < NUMBER_OF_PROCESSORS; i++) {
-							// call create child nodes by giving the id of processor as a parameter
-							// get the returned child solutionNodes
-							SolutionNode childSolutionNode = solutionNode.createChildNode(taskNode, i);
-
-							// call algorithm based on this child solutionNodes
-							DFSBranchAndBoundAlgorithm(childSolutionNode);
-						}
-
-					}
-				}
-			} else {
-
-				// compare the actual time of the leaf to the best time
-				if (solutionNode.getEndTime() < _bestTime) {
-					_bestSolution = solutionNode;
-					_bestTime = solutionNode.getEndTime();
-				}
-			}
-		}
 	}
 
 	public int getTotalTaskWeight() {
--- src/main/java/algorithm/SolutionTree.java
+++ src/main/java/algorithm/SolutionTree.java
@@ -1,15 +1,15 @@
 package algorithm;
 
-import java.util.*;
+import java.util.List;
 
-public class SolutionTree {
-	private int _bestTime = Integer.MAX_VALUE; // best time
-	private SolutionNode _bestSolution;
-	private final List<TaskNode> TASKS;
-	private final SolutionNode ROOT;
-	private final boolean IDENTICAL_TASKS;
-	private final int NUMBER_OF_PROCESSORS;
-	private final int TOTAL_TASK_WEIGHT;
+public abstract class SolutionTree {
+	protected int _bestTime = Integer.MAX_VALUE; // best time
+	protected SolutionNode _bestSolution;
+	protected final List<TaskNode> TASKS;
+	protected final SolutionNode ROOT;
+	protected final boolean IDENTICAL_TASKS;
+	protected final int NUMBER_OF_PROCESSORS;
+	protected final int TOTAL_TASK_WEIGHT;
 
 	public SolutionTree(List<TaskNode> allTasks, List<Processor> processors) {
 		ROOT = new SolutionNode(processors, allTasks);
@@ -44,86 +44,7 @@ public class SolutionTree {
 	 *
 	 * @param solutionNode the node to perform DFS branch and bound
 	 */
-	private void DFSBranchAndBoundAlgorithm(SolutionNode solutionNode) {
-		// Optimisation: HashMap of Tasks to Processors that are to be allocated this round.
-		// If we have two or more TaskNodes that are identical then we only have to schedule them to a processor once.
-		// i.e. if A and B are identical tasks, no need to schedule them on Processor 1.
-		Map<TaskNode, Integer> taskToProcessor = new HashMap<>();
-
-		// check the lower bound (estimation) of this node
-		if (solutionNode.getLowerBound(TOTAL_TASK_WEIGHT) < _bestTime) {
-
-			// get the unvisited nodes of this solutionNode
-			List<TaskNode> unvisitedTaskNodes = solutionNode.getUnvisitedTaskNodes();
-
-			// if this solutionNode still has unvisited task node
-			if (!unvisitedTaskNodes.isEmpty()) {
-				// loop through all the unvisited task nodes
-				for (TaskNode taskNode : unvisitedTaskNodes) {
-
-					// only go through this loop if there is at least one pair of identical tasks
-					if (IDENTICAL_TASKS) {
-						// check in the hashmap if an identical tasknode has been allocated
-						boolean isIdenticalToTask = false;
-						for (TaskNode other : taskToProcessor.keySet()) {
-							if (taskNode.isIdenticalTo(other)) {
-								isIdenticalToTask = true;
-								break;
-							}
-						}
-						if (isIdenticalToTask) continue; // if identical, skip this task
-					}
-
-					// optimisation: if more than one empty processor, only allocate a task to one
-					boolean hasSeenEmpty = false;
-
-					// if this task node can be used to create a partial solution
-					if (!solutionNode.canCreateNode(taskNode)) {
-						break;
-					} else {
-						// find the possible start time for this taskNode
-						solutionNode.calculateStartTime(taskNode);
-
-						// loop through all processors
-						for (int i = 0; i < NUMBER_OF_PROCESSORS; i++) {
-							// if the processor is empty
-							if (solutionNode.getProcessors().get(i).getEndTime() == 0) {
-								if (!hasSeenEmpty) { // first instance of a processor with no tasks
-									// call create child nodes by giving the id of processor as a parameter
-									// get the returned child solutionNodes
-									SolutionNode childSolutionNode = solutionNode.createChildNode(taskNode, i);
-
-									// call algorithm based on this child solutionNodes
-									DFSBranchAndBoundAlgorithm(childSolutionNode);
-
-									// now that we have allocated a task to an empty processor, there is no need
-									// to allocate to another empty processor - eliminating identical states
-									hasSeenEmpty = true;
-								}
-							} else {
-								// call create child nodes by giving the id of processor as a parameter
-								// get the returned child solutionNodes
-								SolutionNode childSolutionNode = solutionNode.createChildNode(taskNode, i);
-
-								// call algorithm based on this child solutionNodes
-								DFSBranchAndBoundAlgorithm(childSolutionNode);
-							}
-
-							taskToProcessor.put(taskNode, i);
-						}
-					}
-				}
-			} else {
-
-				// compare the actual time of the leaf to the best time
-				if (solutionNode.getEndTime() < _bestTime) {
-					_bestSolution = solutionNode;
-					_bestTime = solutionNode.getEndTime();
-				}
-			}
-
-		}
-	}
+	protected abstract void DFSBranchAndBoundAlgorithm(SolutionNode solutionNode);
 
 	/**
 	 * Upon initialisation, will check for identical TaskNodes.
@@ -132,7 +53,7 @@ public class SolutionTree {
 	 * This will set a Hashmap in each task with the key of the identical task, allowing for
 	 * O(1) lookups.
 	 */
-	private boolean markIdenticalTasks() {
+	protected boolean markIdenticalTasks() {
 		boolean identicalTasksFound = false;
 		for (TaskNode taskA : TASKS) {
 			for (TaskNode taskB : TASKS) {
