5eb0ac92c8f8cf946b77e390942267aa81479506
Tommy Shi <tommyshi19991130@gmail.com>
2020-08-29 23:51:35 +1200

    [LUCK-016] Make better test cases to test sequential and parallel

--- src/test/java/main/SchedulerExampleSystemTest.java
+++ src/test/java/main/SchedulerExampleSystemTest.java
@@ -26,11 +26,9 @@ public class SchedulerExampleSystemTest {
     @Test
     public void testNodes7OutTree() {
         _scheduler.main(new String[] {"src/test/resources/Nodes_7_OutTree.dot", "2"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(28, _scheduler.getBestSolution().getEndTime());
 
         _scheduler.main(new String[] {"src/test/resources/Nodes_7_OutTree.dot", "4"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(22, _scheduler.getBestSolution().getEndTime());
     }
 
@@ -40,11 +38,9 @@ public class SchedulerExampleSystemTest {
     @Test
     public void testNodes8Random() {
         _scheduler.main(new String[] {"src/test/resources/Nodes_8_Random.dot", "2"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(581, _scheduler.getBestSolution().getEndTime());
 
         _scheduler.main(new String[] {"src/test/resources/Nodes_8_Random.dot", "4"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(581, _scheduler.getBestSolution().getEndTime());
     }
 
@@ -54,11 +50,9 @@ public class SchedulerExampleSystemTest {
     @Test
     public void testNodes9SeriesParallel() {
         _scheduler.main(new String[] {"src/test/resources/Nodes_9_SeriesParallel.dot", "2"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(55, _scheduler.getBestSolution().getEndTime());
 
         _scheduler.main(new String[] {"src/test/resources/Nodes_9_SeriesParallel.dot", "4"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(55, _scheduler.getBestSolution().getEndTime());
     }
 
@@ -68,11 +62,9 @@ public class SchedulerExampleSystemTest {
     @Test
     public void testNodes10Random() {
         _scheduler.main(new String[] {"src/test/resources/Nodes_10_Random.dot", "2"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(50, _scheduler.getBestSolution().getEndTime());
 
         _scheduler.main(new String[] {"src/test/resources/Nodes_10_Random.dot", "4"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(50, _scheduler.getBestSolution().getEndTime());
     }
 
@@ -82,11 +74,9 @@ public class SchedulerExampleSystemTest {
     @Test
     public void testNodes11OutTree() {
         _scheduler.main(new String[] {"src/test/resources/Nodes_11_OutTree.dot", "2"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(350, _scheduler.getBestSolution().getEndTime());
 
         _scheduler.main(new String[] {"src/test/resources/Nodes_11_OutTree.dot", "4"});
-        assertEquals(SequentialSolutionTree.class, _scheduler.getSolutionTree().getClass());
         assertEquals(227, _scheduler.getBestSolution().getEndTime());
     }
 
